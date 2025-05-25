1629c3e6f269b7ec3b051f0ad71ebf7cd94fd329
Rayca Thais <raycathaisbn@gmail.com>
2025-05-25 17:33:57 -0300

    Adicionando novas Entidades: Emprestimo e Leitor

---
+++ src/main/java/org/serratec/backend/exercicioPaginacaoDTO/domain/Emprestimo.java
@@ -0,0 +1,73 @@
+package org.serratec.backend.exercicioPaginacaoDTO.domain;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+
+public class Emprestimo {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	//fazer o relacionamento 
+	private Livro livro;
+	
+	@ManyToOne
+	@JoinColumn (name = "id_leitor")
+	private Leitor leitor;
+	
+	private LocalDate dataEmprestimo;
+	
+	private LocalDate dataDevolucao;
+	
+	@PrePersist
+	public void setarDataEmprestimo() {
+		this.dataEmprestimo = LocalDate.now();
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Livro getLivro() {
+		return livro;
+	}
+
+	public void setLivro(Livro livro) {
+		this.livro = livro;
+	}
+
+	public Leitor getLeitor() {
+		return leitor;
+	}
+
+	public void setLeitor(Leitor leitor) {
+		this.leitor = leitor;
+	}
+
+	public LocalDate getDataEmprestimo() {
+		return dataEmprestimo;
+	}
+
+	public void setDataEmprestimo(LocalDate dataEmprestimo) {
+		this.dataEmprestimo = dataEmprestimo;
+	}
+
+	public LocalDate getDataDevolucao() {
+		return dataDevolucao;
+	}
+
+	public void setDataDevolucao(LocalDate dataDevolucao) {
+		this.dataDevolucao = dataDevolucao;
+	}
+}
---
+++ src/main/java/org/serratec/backend/exercicioPaginacaoDTO/domain/Leitor.java
@@ -0,0 +1,73 @@
+package org.serratec.backend.exercicioPaginacaoDTO.domain;
+
+import java.util.List;
+
+import org.hibernate.annotations.Where;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public class Leitor {
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank
+	private String nome;
+	
+	@Email
+	private String email;
+	
+	@NotBlank
+	private String senha;
+	
+	@OneToMany(mappedBy = "leitor")
+	@Where(clause = "data_devolucao IS NULL")
+	private List<Emprestimo> emprestimos;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public List<Emprestimo> getEmprestimos() {
+		return emprestimos;
+	}
+
+	public void setEmprestimos(List<Emprestimo> emprestimos) {
+		this.emprestimos = emprestimos;
+	}
+	
+	
+}
